e99acb8ed4f32aa81857ee65ebdfe0f1db5e5c1e
wifelette <LeahSilber@gmail.com>
2020-01-30 22:46:37 -0500

    Done.

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,23 +2,26 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    # includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+    includes(boats: :classifications).where(classifications: { name: "Catamaran" })
   end
 
   def self.sailors
-    # includes(boats: :classifications).where(classifications: {name: "Sailboat"}).distinct
+    includes(boats: :classifications).where(classifications: { name: "Sailboat" }).distinct
+    # This is pretty close to Ruby's .uniq
   end
 
   def self.motorboat_operators
-    # includes(boats: :classifications).where(classifications: {name: "Motorboat"})
+    includes(boats: :classifications).where(classifications: { name: "Motorboat" })
   end
 
   def self.talented_seafarers
-    # where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
+    where("id IN (?)", self.sailors.pluck(:id) & self.motorboat_operators.pluck(:id))
   end
 
   def self.non_sailors
-    # where.not("id IN (?)", self.sailors.pluck(:id))
+    where.not("id IN (?)", self.sailors.pluck(:id))
+    # The second half of this after the comma, is just what goes in place of the ?
+    # self.sailors.pluck(:id) >> Give me an array of sailor IDs
+    # So give me everything where the ID is not in this list
   end
-
 end
